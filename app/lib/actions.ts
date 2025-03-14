3982986188d06d219055ee247ba35c8488f8a6c3
felepo <fecrackle@gmail.com>
2025-03-14 00:53:15 -0600

    Adding create invoice feature

---
+++ app/lib/actions.ts
@@ -0,0 +1,42 @@
+'use server';
+
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+// Define the schema for the form data
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  // Preparing all the data for the database
+  // Extract the customerId, amount, and status from the form data and validate it
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  // Insert the invoice into the database
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  // This will revalidate the page and update the cache
+  revalidatePath('/dashboard/invoices');
+  // This will redirect the user to the invoices page
+  redirect('/dashboard/invoices');
+}
