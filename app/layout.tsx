40e69d6c36f4dbf3e5c70ce368159bbb5e89ef93
felepo <fecrackle@gmail.com>
2025-03-22 12:48:18 -0600

    Adding Basic Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard developed by felepo',
+    default: 'Acme Dashboard developed by felepo',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router by felepo.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
